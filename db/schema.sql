3bcb363129e184ccf8b0602e9ccbe99e8d148bcd
Mr. T <c.austin.terry@gmail.com>
2019-09-16 12:50:18 -0600

    Initial commit and file setup

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db
+
+CREATE TABLE burgers
+(
+    id int
+    AUTO_INCREMENT,
+burger_name VARCHAR
+    (100),
+devoured BOOLEAN;
+PRIMARY KEY
+    (id));
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Shake Your Honeymaker Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bruschetta Bout It Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Pickle My Funny Bone Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Chile Relleno-You-Didn't Burger");
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bohemian Radishy Burger');
