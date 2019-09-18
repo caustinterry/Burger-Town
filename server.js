2a6133b724ae7d62400b0b4a802a4d042865e992
Mr. T <c.austin.terry@gmail.com>
2019-09-17 18:16:16 -0600

    updated some of the initial files

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
 
 var app = express();
 
@@ -11,19 +10,3 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: ""
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.log("error connection: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
