bbbf36bb83c3761c23fac77f40e2104963f4f5a6
Mr. T <c.austin.terry@gmail.com>
2019-09-18 13:31:33 -0600

    Updated the js files, began coding ORM

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,30 @@
-var connection = require("../config/connection");
+var connection = require("../config/connection.js");
+
+var orm = {
+  selectAll: function(tableInput) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput], function(error, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+      res.render("index", { quotes: data });
+    });
+  },
+  insertOne: function(tableInput, tableCol, tableVal) {
+    var queryString = "INSERT INTO ?? (??) VALUES (?)";
+    connection.query(queryString, [tableInput, tableCol, tableVal], function(
+      error,
+      data
+    ) {
+      if (err) throw err;
+    });
+  },
+  updateOne: function(tableInput) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput], function(error, data) {
+      if (err) throw err;
+    });
+  }
+};
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 4000;
 
 var app = express();
-
-var PORT = process.env.PORT || 4000;
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("App listening on: http://localhost:" + PORT);
+});
